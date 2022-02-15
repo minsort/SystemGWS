0bdb1e69051f981eb5f9feb61ef6a92df6f419f9
minsort <71903774+minsort@users.noreply.github.com>
2022-02-15 11:04:32 +0300

    Add files via upload

---
+++ gws/src/main/java/com/example/gws/GwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwsApplication.class, args);
+	}
+
+}
