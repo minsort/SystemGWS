57d3ea637593f0dfbca5a0797d7d53cfb786192a
minsort <71903774+minsort@users.noreply.github.com>
2022-02-18 19:39:19 +0300

    Delete gws directory

--- gws/src/main/java/com/example/gws/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gws.repository;
-
-import com.example.gws.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- gws/src/main/java/com/example/gws/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gws.repository;
-
-import com.example.gws.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
